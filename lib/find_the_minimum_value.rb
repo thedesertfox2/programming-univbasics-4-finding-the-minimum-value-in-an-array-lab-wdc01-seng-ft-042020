97afb3f1ed711f4465f23939e6f6dec3420e9716
carefree-engine-5533 <alexander.gautherin@uconn.edu>
2020-04-10 13:30:48 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -2,10 +2,22 @@ def find_min_value(array)
   count = 0
   min_value = 20
   while count < array[count] do
-    if min_value > array[count] 
+    if min_value <= array[count] 
      puts min_value = array[count]
     end
    count += 1
   end
   return min_value
+end
+
+def find_max_value(array)
+  count = 0
+  max_value = -1
+  while count < array.length() do
+    if max_value < array[count]
+      puts max_value = array[count]
+    end
+   count += 1
+  end
+  return max_value
 end
